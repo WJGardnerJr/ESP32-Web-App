42605fb8431e6ff0fb520c9542484c17782f65cb
WJGardnerJr <wgardner2013@fau.edu>
2024-04-09 16:40:27 -0400

    Fixed auto-reload on clear function.

--- public/app.js
+++ public/app.js
@@ -22,23 +22,29 @@ function refresh() {
 function clearFirestore() {
     // Display confirmation dialog
     if (confirm("Are you sure you want to clear Firestore? This action cannot be undone.")) {
-        const collectionRef = firestore.collection('yourCollectionName');
-        collectionRef.get().then((querySnapshot) => {
-            querySnapshot.forEach((doc) => {
-                collectionRef.doc(doc.id).delete().then(() => {
-                    console.log("Document successfully deleted!");
-                }).catch((error) => {
-                    console.error("Error removing document: ", error);
-                });
-            });
-        }).catch((error) => {
-            console.error("Error getting documents: ", error);
-        });
-    window.location.reload();
-    } else {
-        // User clicked "No", do nothing
-        console.log("Clear action canceled by user.");
-    }
+      var storageRef = firebase.storage().ref();
+      storageRef.listAll().then(result => {
+          if (result.items.length > 0) {
+              let deletePromises = [];
+              result.items.forEach(itemRef => {
+                  // Add the delete promise to an array
+                  deletePromises.push(itemRef.delete());
+              });
+  
+              // Wait for all files to be deleted
+              Promise.all(deletePromises).then(() => {
+                  console.log('All files deleted');
+                  window.location.reload(); // Reload the page after deleting the files
+              }).catch(error => {
+                  console.error("Error deleting files: ", error);
+              });
+          } else {
+              console.log('No files to delete in Storage');
+          }
+      }).catch(error => {
+          console.error("Error listing files in Storage: ", error);
+      });
+  }
 }
 
 function deleteFile(directoryRef, fileName) {
